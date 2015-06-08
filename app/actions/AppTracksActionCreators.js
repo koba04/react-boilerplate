ec1723b6833bcac6ef92e0de92adbfcca0fe6709
Toru Kobayashi <koba0004@gmail.com>
2015-06-08 14:14:49 +0900

    Remove VIEW_ACTION

--- app/actions/AppTracksActionCreators.js
+++ app/actions/AppTracksActionCreators.js
@@ -1,8 +1,10 @@
 import request        from 'superagent';
 import AppDispatcher  from '../dispatcher/AppDispatcher';
-import AppConstants   from '../constants/AppConstants';
+import {
+  RECEIVE_TRACKS_BY_ARTIST,
+  RECEIVE_TRACKS_BY_COUNTRY
+} from '../constants/AppConstants';
 
-const ActionTypes = AppConstants.ActionTypes;
 const urlRoot = "http://ws.audioscrobbler.com/2.0/?api_key=b867bf0fdfe95e6c6dc31a275535f765&format=json&";
 
 // TODO Loading
@@ -10,8 +12,8 @@ export default {
   fetchByArtist(artist) {
     request.get(`${urlRoot}method=artist.gettoptracks&artist=${encodeURIComponent(artist)}`)
     .end((err, res) => {
-      AppDispatcher.handleViewAction({
-        type: ActionTypes.RECEIVE_TRACKS_BY_ARTIST,
+      AppDispatcher.dispatch({
+        type: RECEIVE_TRACKS_BY_ARTIST,
         tracks: res.body.toptracks.track
       });
     });
@@ -19,8 +21,8 @@ export default {
   fetchByCountry(country) {
     request.get(`${urlRoot}method=geo.gettoptracks&country=${encodeURIComponent(country)}`)
     .end((err, res) => {
-      AppDispatcher.handleViewAction({
-        type: ActionTypes.RECEIVE_TRACKS_BY_ARTIST,
+      AppDispatcher.dispatch({
+        type: RECEIVE_TRACKS_BY_ARTIST,
         tracks: res.body.toptracks.track
       });
     });
--- app/dispatcher/AppDispatcher.js
+++ app/dispatcher/AppDispatcher.js
@@ -1,14 +1,3 @@
 import {Dispatcher} from 'flux';
-import assign       from 'object-assign';
-import AppConstants from '../constants/AppConstants';
 
-const PayloadSources = AppConstants.PayloadSources;
-
-export default assign(new Dispatcher(), {
-  handleViewAction(action) {
-    this.dispatch({
-      source: PayloadSources.VIEW_ACTION,
-      action: action
-    });
-  }
-});
+export default new Dispatcher();
